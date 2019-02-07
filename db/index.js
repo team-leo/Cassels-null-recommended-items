424fb6b430b5f51646a810f1e79308eceb9debb3
Hack Reactor Student <student@hackreactor.com>
2019-02-06 21:43:57 -0500

    Seed database with random data

--- db/index.js
+++ db/index.js
@@ -7,6 +7,7 @@ let db = new sqlite.Database(path.join(__dirname, "./products.db"), (err)=>{
         console.log("ERROR in db/index.js when connecting: "+err);
     } else {
         console.log("connected to SQLite database");
+        seedData(100);
     }
 });
 
@@ -19,13 +20,17 @@ let db = new sqlite.Database(path.join(__dirname, "./products.db"), (err)=>{
 //     console.log(`A row has been inserted with rowid ${this.lastID}`);
 // });
 
-var vals = ['Brown Pants', 4.5, 1337, "$41.99", 'https://images-na.ssl-images-amazon.com/images/I/31VU-ryYsML._AC_UL260_SR200,260_.jpg', 0]
-vals[0] = faker.commerce.productName();
-vals[1] = Math.floor((Math.random()*10))/2;
-vals[2] = Math.floor((Math.random()*Math.random()*10000));
+//var vals = ['Brown Pants', 4.5, 1337, "$41.99", 'https://images-na.ssl-images-amazon.com/images/I/31VU-ryYsML._AC_UL260_SR200,260_.jpg', 0]
 
-function randomVals(vals){
-    vals[0] = faker.fake.name;
+function randomVal(){
+    var vals = [];
+    vals[0] = faker.commerce.productName();
+    vals[1] = Math.floor((Math.random()*10))/2;
+    vals[2] = Math.floor((Math.random()*Math.random()*10000));
+    vals[3] = faker.commerce.price()+"";
+    vals[4] = faker.image.imageUrl();
+    vals[5] = Math.round(Math.random());
+    return vals
 }
 
 function addRow(vals){
@@ -39,4 +44,9 @@ function addRow(vals){
     });
 }
 
-addRow(vals);
+function seedData(n){
+    for(i=0; i<n; i++){
+        addRow(randomVal())
+    }
+}
+
