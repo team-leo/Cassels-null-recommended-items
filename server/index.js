a0a3d6992a067feaeafc0db5ba31de2da2c48ac8
Daniel Maziad <dmaziad@gmail.com>
2019-03-11 22:29:12 -0400

    bundle url aws with port

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(function(req, res, next) {
   });
 
 
-
+app.get('/api/recs', controllers.fetchBundle)
 app.get('/:itemId', (req, res) => {
     res.sendFile(path.join(__dirname + '/../client/dist/index.html'))
 })
